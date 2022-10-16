a84a485aa3b83423d7c310d53f56f5f96cc7b1ee
Raul Sarbu <raul.sarbu28@gmail.com>
2022-10-17 00:30:46 +0300

    added context and migrations

--- quizzdos-be/Program.cs
+++ quizzdos-be/Program.cs
@@ -1,12 +1,17 @@
+global using gps_manager.EFCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<ManagerContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
