7c3e150c3085e7882f50b40ce19715af9321014f
Raul Sarbu <raul.sarbu28@gmail.com>
2022-10-24 00:14:29 +0300

    Added Responses

---
+++ quizzdos-be/Responses/EmailValidation/EmailValidationResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace quizzdos_be.Responses.EmailValidation
+{
+    public class EmailValidationResponse
+    {
+        public bool EmailLengthOkay { get; set; } = true;
+        public string EmailLengthMessage { get; set; } = string.Empty;
+        public bool EmailFormatOkay { get; set; } = true;
+        public string EmailFormatMessage { get; set; } = string.Empty;
+        public bool EmailExists { get; set; } = true;
+        public string EmailExistsMessage { get; set; } = string.Empty;
+        
+    }
+}
