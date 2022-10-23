0cda195e8b48c40fda6484396bf42dd32cf6dacb
Raul Sarbu <raul.sarbu28@gmail.com>
2022-10-24 00:14:47 +0300

    Added Auth Controller, Repo and Validation

--- quizzdos-be/Program.cs
+++ quizzdos-be/Program.cs
@@ -1,5 +1,6 @@
 global using gps_manager.EFCore;
 using Microsoft.EntityFrameworkCore;
+using quizzdos_be.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ManagerContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IValidationRepository, ValidationRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
