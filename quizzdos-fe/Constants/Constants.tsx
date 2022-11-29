62d13660a9ac2966ed5a310c4de4a56ecd136774
Raul Sarbu <raul.sarbu28@gmail.com>
2022-11-29 16:59:36 +0200

    Added temporary constant to constants to test things out

--- quizzdos-fe/Constants/Constants.tsx
+++ quizzdos-fe/Constants/Constants.tsx
@@ -1,4 +1,12 @@
 export const Constants = {
 	blueLogo: "#9fa0f4",
-    darkBackround: "#282c34",
+	darkBackround: "#282c34",
+}
+
+export const ApiConstants = {
+	baseUrl: "http://192.168.0.177:5000/api/",
+	auth: "Auth/",
+	login: "http://192.168.0.177:5000/api/Auth/Login",
+	register: "Register",
+	user: "http://192.168.0.177:5000/api/User/User",
 }
