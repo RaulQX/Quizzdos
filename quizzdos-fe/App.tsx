a2c784e22a4d1c4c8457909ce11accfb858058e7
Raul Sarbu <raul.sarbu28@gmail.com>
2022-11-29 16:59:56 +0200

    Changed components to use context

--- quizzdos-fe/App.tsx
+++ quizzdos-fe/App.tsx
@@ -1,6 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import React from "react"
+import { UserProvider } from "./components/context/User/UserContext"
+import Home from "./screens/Home"
 import Login from "./screens/Login"
 import Register from "./screens/Register"
 import Welcome from "./screens/Welcome"
@@ -9,16 +11,18 @@ const Stack = createNativeStackNavigator()
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false,
-				}}
-			>
-				<Stack.Screen name="Welcome" component={Welcome} />
-				<Stack.Screen name="Login" component={Login} />
-				<Stack.Screen name="Register" component={Register} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<UserProvider>
+			<NavigationContainer>
+				<Stack.Navigator
+					screenOptions={{
+						headerShown: false,
+					}}
+				>
+					<Stack.Screen name="Welcome" component={Welcome} />
+					<Stack.Screen name="Login" component={Login} />
+					<Stack.Screen name="Register" component={Register} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</UserProvider>
 	)
 }
