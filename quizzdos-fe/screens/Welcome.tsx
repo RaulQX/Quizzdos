4d74dbac55243cf795dfecaf07c905cab402a549
Raul Sarbu <raul.sarbu28@gmail.com>
2022-12-01 15:22:24 +0200

    welcome-screen-styling

--- quizzdos-fe/screens/Welcome.tsx
+++ quizzdos-fe/screens/Welcome.tsx
@@ -8,26 +8,26 @@ import Logo from "../components/common/Logo"
 const Welcome = ({ navigation }: any) => {
 	return (
 		<View
-			style={s`h-full flex flex-col justify-center items-center bg-coolGray-700`}
+			style={s`h-full flex flex-col justify-start items-center bg-coolGray-700`}
 		>
-			<Logo />
-			<View style={s`flex flex-col w-full p-6`}>
-				<ButtonImportant
-					text="Login"
-					onPress={() => navigation.navigate("Login")}
-				/>
-				<View style={s`mt-4`}>
+			<View style={s`h-1/3 justify-end`}>
+				<Logo />
+			</View>
+			<View style={s`flex flex-col justify-end w-full h-1/2`}>
+				<View style={s`flex flex-col w-full p-6`}>
 					<ButtonImportant
-						text="Register"
-						onPress={() => {
-							navigation.navigate("Register")
-						}}
+						text="Login"
+						onPress={() => navigation.navigate("Login")}
 					/>
+					<View style={s`mt-4`}>
+						<ButtonImportant
+							text="Register"
+							onPress={() => {
+								navigation.navigate("Register")
+							}}
+						/>
+					</View>
 				</View>
-				<ButtonImportant
-					text="Home"
-					onPress={() => navigation.navigate("Home")}
-				/>
 			</View>
 		</View>
 	)
