0d86b9bee7c533a556b6d54dc4a72cce2e2d168c
Raul Sarbu <68293897+RaulQX@users.noreply.github.com>
2023-01-01 20:15:17 +0200

    styled header

--- quizzdos-fe/components/common/Header.tsx
+++ quizzdos-fe/components/common/Header.tsx
@@ -4,13 +4,18 @@ import { s } from "react-native-wind"
 const HeaderTitle = ({ title }: headerProps) => {
 	const splitTitle = title.split(" ")
 	return (
-		<View
-			style={s`flex flex-row justify-center mt-4 mb-4 border-b-2 border-gray-300 w-full`}
-		>
-			<Text style={s`text-3xl text-white `}>
-				{splitTitle[0]}
-				<Text style={s`text-indigo-300`}> {splitTitle[1]}</Text>
-			</Text>
+		<View style={s`flex w-full items-center justify-center`}>
+			<View
+				style={[
+					s`flex flex-row justify-center align-center mt-4 mb-4 border-b-2 border-gray-300 w-11/12`,
+					{ boxShadow: "0px 3px 0px rgba(0,0,0,.15)" },
+				]}
+			>
+				<Text style={s`text-3xl text-white mb-2`}>
+					{splitTitle[0]}
+					<Text style={s`text-indigo-300`}> {splitTitle[1]}</Text>
+				</Text>
+			</View>
 		</View>
 	)
 }
